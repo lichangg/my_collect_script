309683ef8148a5482c41942a855dc8b911593778
Chenggang Li <chenggang.li@shopee.com>
2025-06-23 18:37:55 +0800

    add

--- web_js/web_hook.js
+++ web_js/web_hook.js
@@ -66,3 +66,63 @@ Object.defineProperty(obj.headers, 'Anti-Content', {
     configurable: true,
     enumerable: true
 });
+
+// 监控canvas的使用，一般现在收集指纹都会获取这个，该脚本能快速找到收集的代码位置，进而找到vmp运行位置做插桩
+(function() {
+    // Helper function to create a hook
+    function hookMethod(obj, methodName) {
+        const originalMethod = obj[methodName];
+        Object.defineProperty(obj, methodName, {
+            configurable: true,
+            enumerable: false,
+            writable: false,
+            value: function(...args) {
+                console.log(`[HOOKED] ${methodName} called with arguments:`, args);
+                debugger; // Add a breakpoint here
+                return originalMethod.apply(this, args);
+            }
+        });
+    }
+
+    // Hook methods of HTMLCanvasElement
+    const canvasMethods = [
+        'getContext', // Commonly used to retrieve 2D/WebGL contexts
+        'toDataURL',  // Used to extract image data
+        'toBlob'      // Used to extract image data
+    ];
+    canvasMethods.forEach(method => hookMethod(HTMLCanvasElement.prototype, method));
+
+    // Hook methods of CanvasRenderingContext2D
+    const canvas2DMethods = [
+        'fillText', 'strokeText', 'measureText', 'drawImage',
+        'getImageData', 'putImageData', 'createLinearGradient', 'createPattern',
+        'createRadialGradient'
+    ];
+    canvas2DMethods.forEach(method => hookMethod(CanvasRenderingContext2D.prototype, method));
+
+    // Hook methods of WebGLRenderingContext
+    const webGLMethods = [
+        'drawArrays', 'drawElements', 'getShaderPrecisionFormat', 'readPixels',
+        'getExtension', 'getParameter'
+    ];
+    webGLMethods.forEach(method => hookMethod(WebGLRenderingContext.prototype, method));
+
+    // Hook OffscreenCanvas (if supported)
+    if (typeof OffscreenCanvas !== 'undefined') {
+        hookMethod(OffscreenCanvas.prototype, 'getContext');
+    }
+
+    // Observe creation of new canvases dynamically
+    const originalCreateElement = document.createElement;
+    document.createElement = function(tagName, ...args) {
+        if (tagName.toLowerCase() === 'canvas') {
+            console.log('[HOOKED] Canvas element created');
+            debugger; // Add a breakpoint here
+        }
+        return originalCreateElement.call(document, tagName, ...args);
+    };
+
+    console.log('Canvas hooks successfully installed.');
+})();
+
+
